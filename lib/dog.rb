d2195c38bb03457e9b075ccf05e557d6448877cb
Miluche <meshkipla@gmail.com>
2023-02-27 21:25:26 +0300

    implemented a dog class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,14 @@
-class Dog
+class Dog < ActiveRecord::Base
+=begin
+  attr_accessor :name
+  attr_accessor :breed
+=end
+
+=begin
+  def initialize(name:, breed:)
+    @name = name
+    @breed = breed
+  end
+=end
+
 end
